b5e0b4511ad05f5d6a9c2bb9a3d3d4bba8a856b4
emma-deleon <e.mmadeleonk04@gmail.com>
2026-04-27 10:56:01 +0800

    making thingsss

--- Assets/Starter Assets/Runtime/Mobile/Scripts/VirtualInput.cs
+++ Assets/Starter Assets/Runtime/Mobile/Scripts/VirtualInput.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using StarterAssets;
 using UnityEngine;
 
@@ -27,33 +26,3 @@ public class VirtualInput : MonoBehaviour
         StarterAssetsInputs.SprintInput(virtualSprintState);
     }
 }
-=======
-using StarterAssets;
-using UnityEngine;
-
-public class VirtualInput : MonoBehaviour
-{
-    [Header("Output")]
-    public StarterAssetsInputs StarterAssetsInputs;
-
-    public void VirtualMoveInput(Vector2 virtualMoveDirection)
-    {
-        StarterAssetsInputs.MoveInput(virtualMoveDirection);
-    }
-
-    public void VirtualLookInput(Vector2 virtualLookDirection)
-    {
-        StarterAssetsInputs.LookInput(virtualLookDirection);
-    }
-
-    public void VirtualJumpInput(bool virtualJumpState)
-    {
-        StarterAssetsInputs.JumpInput(virtualJumpState);
-    }
-
-    public void VirtualSprintInput(bool virtualSprintState)
-    {
-        StarterAssetsInputs.SprintInput(virtualSprintState);
-    }
-}
->>>>>>> cf1216420f77afb438990ea80c532b5b1c4209c5
